6b61d24e004ba0ea49f32aed6d0ce11ba1fd0343
skintifique <78672198+skintifique@users.noreply.github.com>
2021-03-15 18:42:47 +0100

    Update module_gotoamzbtn_101.js

--- module_gotoamzbtn_101.js
+++ module_gotoamzbtn_101.js
@@ -24,17 +24,10 @@ function find_amz_onetag (x) {
 let product_asin_elmt = document.getElementById("product_asin") ;
 let product_asin ;
 if (!product_asin_elmt) { 
-	product_asin = "unavailable" ;
+    product_asin = "unavailable" ;
     } else {
     product_asin = product_asin_elmt.value ;
     }
-let amz_onetag ;
-if (x == "skintifique0-20") {
-amz_onetag = "skintifique0-20";
-} else {
-amz_onetag = "skintifique0-20";
-}
-let amz_url ;
 let amz_shop ;
 let url = window.location.href ;
 if (url.includes("/en/")) {
@@ -43,7 +36,22 @@ if (url.includes("/en/")) {
 	amz_shop = "https://www.amazon.fr" ;
 } else {	
 	amz_shop = "https://www.amazon.com" ;
+}	
+let amz_onetag ;
+if (x == "skintifique00-20") {
+amz_onetag = "skintifique00-20";
+} else if (x == "skintifique-21") {
+amz_onetag = "skintifique-21";
+} else {
+if (url.includes("/en/")) {
+	amz_onetag = "skintifique00-20" ;
+} else if (url.includes("/fr/")) {
+	amz_onetag = "skintifique-21" ;
+} else {	
+	amz_onetag = "skintifique00-20" ;
+}	
 }
+let amz_url ;
 if (product_asin == "unavailable") { 
 	amz_url = amz_shop + "/s?k=skintifique&tag=" + amz_onetag ;
     } else {
