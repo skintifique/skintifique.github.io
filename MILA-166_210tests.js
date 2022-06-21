5b9e1a76fc2e6cfbab78ef8101fd0ebd1e768eb5
skintifique <78672198+skintifique@users.noreply.github.com>
2022-06-21 22:08:20 +0200

    Update MILA-166_210tests.js

--- MILA-166_210tests.js
+++ MILA-166_210tests.js
@@ -4,7 +4,7 @@ let current_urlMILA = window.location.href ;
 //the lines of code above and below are just for when the script is called externally. they need to be removed if plugged into the js theme again
 if (current_urlMILA.includes("/166-mila")) {
 //////show mila-166 version
- document.getElementById("milaJsVs").innerHTML = "THIS PAGE IS IN DEVELOPMENT / CETTE PAGE EST EN DEVELOPPEMENT 29" ;
+ document.getElementById("milaJsVs").innerHTML = "THIS PAGE IS IN DEVELOPMENT / CETTE PAGE EST EN DEVELOPPEMENT 30" ;
 
 
 //function to select product and other options
@@ -35,46 +35,22 @@ if ((selectedOption === "recoOption") || (selectedOption === "profileOption") ||
 //hide the text line under the topOptions as it is now not useful to the user
 document.getElementById("textUnderTopOptions").style.display = "none" ;
 //remove borders for all the top option buttons
-document.getElementById("recoOption").classList.remove("topOption_selected") ;
-document.getElementById("profileOption").classList.remove("topOption_selected") ;
-document.getElementById("diagOption").classList.remove("option_selected1") ;
+///////////////////document.getElementById("recoOption").classList.remove("topOption_selected") ;
+///////////////////document.getElementById("profileOption").classList.remove("topOption_selected") ;
+///////////////////document.getElementById("diagOption").classList.remove("option_selected1") ;
 //reset all the OptionText1 to display block
-document.getElementById("recoOptionText1").style.display = "block";
-document.getElementById("diagOptionText1").style.display = "block";
-document.getElementById("profileOptionText1").style.display = "block";
+///////////////////document.getElementById("recoOptionText1").style.display = "block";
+///////////////////document.getElementById("diagOptionText1").style.display = "block";
+///////////////////document.getElementById("profileOptionText1").style.display = "block";
 //reset all the OptionText2 to display none
-document.getElementById("recoOptionText2").style.display = "none";
-document.getElementById("diagOptionText2").style.display = "none";
-document.getElementById("profileOptionText2").style.display = "none";
+///////////////////document.getElementById("recoOptionText2").style.display = "none";
+///////////////////document.getElementById("diagOptionText2").style.display = "none";
+///////////////////document.getElementById("profileOptionText2").style.display = "none";
 //reset all the textUnderMILAxxxBtn to display none
-document.getElementById("textUnderMILAprofileBtn").style.display = "none";
-document.getElementById("textUnderMILAdiagBtn").style.display = "none";
-document.getElementById("textUnderMILArecoBtn").style.display = "none";
+///////////////////document.getElementById("textUnderMILAprofileBtn").style.display = "none";
+///////////////////document.getElementById("textUnderMILAdiagBtn").style.display = "none";
+///////////////////document.getElementById("textUnderMILArecoBtn").style.display = "none";
 
-//highlight selectedOption and do the other stuff
-////////////////////switch (selectedOption) {
-////////////////////case "recoOption" :
-////////////////////document.getElementById(selectedOption).classList.add("topOption_selected") ;
-////////////////////document.getElementById("recoOption_button").style.opacity = "1" ;
-////////////////////document.getElementById("profileOption_button").style.opacity = "0.3" ;
-//hide the text under the GoAnalyse btn and hide the MILA analysis
-
-
-//show the top of the sliderfor the selectedOption class
-////////////////////document.getElementsByClassName("accordionReco")[0].style.display = "block" ;
-////////////////////document.getElementsByClassName("accordionReco")[1].style.display = "block" ;
-////////////////////document.getElementsByClassName("accordionReco")[2].style.display = "block" ;
-////////////////////document.getElementsByClassName("accordionReco")[3].style.display = "block" ;
-//show the first slide for the selectedOption class and reset the slideIndex
-
-////////////////////document.getElementsByClassName("slideReco")[0].style.display = "block" ;
-//replace recoOptionText1 by recoOptionText2
-////////////////////document.getElementById("recoOptionText1").style.display = "none" ;
-////////////////////document.getElementById("recoOptionText2").style.display = "block" ;
-//store the selectedTopOption
-////////////////////document.getElementById("selectedTopOption").innerHTML = "recoOption" ;
-////////////////////break;
-////////////////////}
 }
 
 });
