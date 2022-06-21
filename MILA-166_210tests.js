4371a0e87732a05feeb12392b89e223f2fc528a4
skintifique <78672198+skintifique@users.noreply.github.com>
2022-06-21 18:53:44 +0200

    Update MILA-166_210tests.js

--- MILA-166_210tests.js
+++ MILA-166_210tests.js
@@ -4,7 +4,7 @@ let current_urlMILA = window.location.href ;
 //the lines of code above and below are just for when the script is called externally. they need to be removed if plugged into the js theme again
 if (current_urlMILA.includes("/166-mila")) {
 //////show mila-166 version
- document.getElementById("milaJsVs").innerHTML = "THIS PAGE IS IN DEVELOPMENT / CETTE PAGE EST EN DEVELOPPEMENT 11" ;
+ document.getElementById("milaJsVs").innerHTML = "THIS PAGE IS IN DEVELOPMENT / CETTE PAGE EST EN DEVELOPPEMENT 12" ;
 ///////////////////////////
 //END get the stored profile parameters and set up all the options
 //////////////////////////////////
