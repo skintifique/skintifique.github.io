48b940428f861794116e06cbcf7401b7e3395d65
skintifique <78672198+skintifique@users.noreply.github.com>
2022-06-21 21:45:59 +0200

    Update MILA-166_210tests.js

--- MILA-166_210tests.js
+++ MILA-166_210tests.js
@@ -4,7 +4,7 @@ let current_urlMILA = window.location.href ;
 //the lines of code above and below are just for when the script is called externally. they need to be removed if plugged into the js theme again
 if (current_urlMILA.includes("/166-mila")) {
 //////show mila-166 version
- document.getElementById("milaJsVs").innerHTML = "THIS PAGE IS IN DEVELOPMENT / CETTE PAGE EST EN DEVELOPPEMENT 24" ;
+ document.getElementById("milaJsVs").innerHTML = "THIS PAGE IS IN DEVELOPMENT / CETTE PAGE EST EN DEVELOPPEMENT 26" ;
 
 
 //function to select product and other options
@@ -15,11 +15,11 @@ let e = selectProduct.target ;
 let selectedOption = e.id ;
 let selectedOption_class = e.classList ;
 //manage situations when user clicks on text below the top optoin images
-if ((selectedOption === "recoOptionText00") || (selectedOption === "recoOptionText01") || (selectedOption === "recoOptionText02")){
-selectedOption = "recoOption" ;
-} else if ((selectedOption === "profileOptionText0") || (selectedOption === "profileOptionText01") || (selectedOption === "profileOptionText02")){
-selectedOption = "profileOption" ;
-}
+//////////////////if ((selectedOption === "recoOptionText00") || (selectedOption === "recoOptionText01") || (selectedOption === "recoOptionText02")){
+//////////////////selectedOption = "recoOption" ;
+//////////////////} else if ((selectedOption === "profileOptionText0") || (selectedOption === "profileOptionText01") || (selectedOption === "profileOptionText02")){
+//////////////////selectedOption = "profileOption" ;
+//////////////////}
 //END manage situations when user clicks on text below the top optoin images
 //manage situations when user clicks on element that is a 1st child of an option
 
@@ -51,29 +51,21 @@ document.getElementById("textUnderMILAprofileBtn").style.display = "none";
 document.getElementById("textUnderMILAdiagBtn").style.display = "none";
 document.getElementById("textUnderMILArecoBtn").style.display = "none";
 //hide all accordion elements
-let accordionElmt = document.getElementsByClassName("accordionElmt") ;
-var j;
-for (j = 0; j < accordionElmt.length; j++) {
-accordionElmt[j].style.display = "none";
-}
-//hide iframeMILA and other elements
+//////////////////let accordionElmt = document.getElementsByClassName("accordionElmt") ;
+//////////////////var j;
+//////////////////for (j = 0; j < accordionElmt.length; j++) {
+//////////////////accordionElmt[j].style.display = "none";
+//////////////////}
 
-//////////////////document.getElementsByClassName("iframeMILA")[0].style.display = "none" ;
-//////////////////document.getElementsByClassName("iframeMILA")[1].style.display = "none" ;
-//////////////////document.getElementsByClassName("iframeMILA")[2].style.display = "none" ;
-//show elements for the personal diag questions
-//////////////////document.getElementsByClassName("prevnextbuttons")[0].style.display = "block" ;
 //highlight selectedOption and do the other stuff
 switch (selectedOption) {
-
 case "recoOption" :
-
 document.getElementById(selectedOption).classList.add("topOption_selected") ;
 document.getElementById("recoOption_button").style.opacity = "1" ;
 document.getElementById("profileOption_button").style.opacity = "0.3" ;
 //hide the text under the GoAnalyse btn and hide the MILA analysis
 //iframeMILA.style.display = "none" ;
-document.getElementById("textUnderMILArecoBtn").style.display = "none" ;
+//////////////////document.getElementById("textUnderMILArecoBtn").style.display = "none" ;
 
 //show the top of the sliderfor the selectedOption class
 document.getElementsByClassName("accordionReco")[0].style.display = "block" ;
@@ -81,7 +73,7 @@ document.getElementsByClassName("accordionReco")[1].style.display = "block" ;
 document.getElementsByClassName("accordionReco")[2].style.display = "block" ;
 document.getElementsByClassName("accordionReco")[3].style.display = "block" ;
 //show the first slide for the selectedOption class and reset the slideIndex
-var slideIndex = 1;
+//////////////////var slideIndex = 1;
 document.getElementsByClassName("slideReco")[0].style.display = "block" ;
 //replace recoOptionText1 by recoOptionText2
 document.getElementById("recoOptionText1").style.display = "none" ;
@@ -91,22 +83,7 @@ document.getElementById("selectedTopOption").innerHTML = "recoOption" ;
 break;
 }
 }
-
 //ENDoptions in ingredient slide
-//options in recommendation section
-//////////////////let accordionSolution = document.getElementsByClassName("accordionSolution") ;
-//////////////////let diag_solution = document.getElementsByClassName("diag_solution") ;
-////////////////// let diag_moisturizer = document.getElementsByClassName("diag_moisturizer") ;
-////////////////// let diag_cleanser = document.getElementsByClassName("diag_cleanser") ;
-
-//select condition_nickel if user is reactive to pollution, shoes, jewelry etc
-//////////////////let allergicToFactor_pollution_classList = document.getElementById("allergicToFactor_pollution").classList ;
-//////////////////let allergicToFactor_jewelry_classList = document.getElementById("allergicToFactor_jewelry").classList ;
-//////////////////let allergicToFactor_leather_classList = document.getElementById("allergicToFactor_leather").classList ;
-//////////////////let allergicToFactor_construction_classList = document.getElementById("allergicToFactor_construction_material").classList ;
-//////////////////if ((allergicToFactor_pollution_classList.contains("option_selectedAllergicToFactor")) || (allergicToFactor_jewelry_classList.contains("option_selectedAllergicToFactor")) || (allergicToFactor_leather_classList.contains("option_selectedAllergicToFactor")) || (allergicToFactor_construction_classList.contains("option_selectedAllergicToFactor"))) {
-//////////////////document.getElementById("condition_nickel_allergy").classList.add("option_selectedCondition") ;
-//////////////////}
 });
 }
 // END OF SCRIPT FOR MILA PAGE
