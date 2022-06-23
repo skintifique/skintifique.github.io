dd89927bb35f8d15711e3c570aa94b1e04caa091
skintifique <78672198+skintifique@users.noreply.github.com>
2022-06-23 10:27:29 +0200

    Update MILA-166_210tests.js

--- MILA-166_210tests.js
+++ MILA-166_210tests.js
@@ -7,17 +7,17 @@ if (current_urlMILA.includes("/166-mila")) {
  document.getElementById("milaJsVs").innerHTML = "THIS PAGE IS IN DEVELOPMENT / CETTE PAGE EST EN DEVELOPPEMENT 34" ;
 
 //function to select product and other options
-document.body.addEventListener("click", function(selectProduct) {
+///////document.body.addEventListener("click", function(selectProduct) {
 ////////////document.getElementById("recoOption").addEventListener("click", function(selectProduct) {
-let e = selectProduct.target ;
-let e_style = getComputedStyle(e) ;
-let e_cursor = e_style.cursor;
+///////let e = selectProduct.target ;
+///////let e_style = getComputedStyle(e) ;
+///////let e_cursor = e_style.cursor;
 
-if (e_cursor === "pointer") {
+///////if (e_cursor === "pointer") {
 
 //script to select options
-let selectedOption = e.id ;
-let selectedOption_class = e.classList ;
+///////let selectedOption = e.id ;
+///////let selectedOption_class = e.classList ;
 //manage situations when user clicks on text below the top optoin images
 //////////////////if ((selectedOption === "recoOptionText00") || (selectedOption === "recoOptionText01") || (selectedOption === "recoOptionText02")){
 //////////////////selectedOption = "recoOption" ;
@@ -33,8 +33,8 @@ let selectedOption_class = e.classList ;
 //////////////////selectedOption = selectedOption_parent ;
 //////////////////}
 /+++
-}
+///////}
 /+++
-});
+///////});
 }
 // END OF SCRIPT FOR MILA PAGE
