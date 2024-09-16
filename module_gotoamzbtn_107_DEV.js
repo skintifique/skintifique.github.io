f4ad543ae3c9530a768a30442536d5cadea34932
skintifique <78672198+skintifique@users.noreply.github.com>
2024-09-16 17:51:58 +0200

    Update module_gotoamzbtn_107_DEV.js
    Handle Prestashop 1.8

--- module_gotoamzbtn_107_DEV.js
+++ module_gotoamzbtn_107_DEV.js
@@ -90,7 +90,10 @@ let product_referenceAff = document.getElementsByClassName("product-reference") 
 // ligne de code ci-dessous est ok mais pas quand il y a plusieurs elements avec class product-reference	
 //let product_referenceAff2 = product_referenceAff[0].querySelector("[itemprop=sku]").textContent ;
 // code ci-dessous pour tolerer les situations ou il y a plusieurs elements avec class product-reference
-let product_referenceAff2 = document.querySelector("[itemprop=sku]").textContent ;
+//let product_referenceAff2 = document.querySelector("[itemprop=sku]").textContent ;
+
+// new query to handle Prestashop 8
+let product_referenceAff2 = document.querySelector("#product-details > div.product-reference > span").innerHTML ;
 	
 
 const skp_productCode = {SKP2200616:"LCx1",SKP2200619:"LSx1",SKP2200644:"MBx1",SKP2200645:"MATU_pack",SKP2200631:"EXEE_body_pack",SKP1300002:"ML200x1",SKP1300003:"CL200x1",SKP1400007:"PC20x1",SKP1400010:"HG40x1",SKP1700310:"HG150x1",SKP1400121:"DISC_pack",SKP2200647:"PSO_balm_pack",SKP1400122:"PSO_face_pack",SKP1400123:"GH40x3",SKP1400124:"ML200x2",SKP1400125:"ML200x3",SKP1400177:"ACNE_pack",SKP1600129:"ECZE_face_pack",SKP1600130:"NAL_pack",SKP1700150:"SEB_pack",SKP1700215:"CL200x3",SKP1700317:"PC20x3",SKP1700408:"PSO_body_pack",SKP1700420:"HG150x3",SKP2100403:"MATE_pack",SKP2100514:"ECZE_body_pack",SKP2100615:"DYSH_pack"} ;
